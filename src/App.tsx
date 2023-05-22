77c5eb5c762f7b5b0b310f3716cebcf759080566
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2023-05-22 07:16:24 +0300

    feat: add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import { LoginPage } from './pages/LoginPage/LoginPage';
+
 import './App.css';
 
-const App = () => <div className='App'>App</div>;
+const App = () => <LoginPage />;
 
 export default App;
