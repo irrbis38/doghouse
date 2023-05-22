3727bff8329f9d772b294ba73af6fb275c789fd6
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-05-22 13:22:34 +0300

    feat: add button component

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,3 +1,26 @@
 import React from 'react';
 
-export const LoginPage = () => <div className='loginpage'>LoginPage</div>;
+import { Button } from '../../common/buttons';
+
+import './LoginPage.css';
+
+export const LoginPage = () => (
+  <div className='login-page'>
+    <div className='login-page__container'>
+      <div>header</div>
+      <div className='login-page__form '>
+        <div>
+          <input type='text' />
+          <span>validation</span>
+        </div>
+        <div>
+          <input type='text' />
+          <span>validation</span>
+        </div>
+        <div>
+          <Button>SignIn</Button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
